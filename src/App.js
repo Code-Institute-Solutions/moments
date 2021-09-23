63bda6ce7cc4a3b0dd71d20079b65c8ab46b36ba
mr-fibonacci <adamlapinski1@gmail.com>
2021-09-23 10:27:57 +0000

    03 bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import Button from "react-bootstrap/Button";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello, World!</h1>
+      <Button variant="primary">Primary</Button>
     </div>
   );
 }
