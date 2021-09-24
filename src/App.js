0ce133afbe70840e36b10daa8e57e132a480a645
mr-fibonacci <adamlapinski1@gmail.com>
2021-09-24 09:40:56 +0000

    04b adding CSS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import "./App.css";
+import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
       <NavBar />
     </div>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.png";
+import styles from "../styles/NavBar.module.css";
 
 const NavBar = () => {
   return (
-    <Navbar expand="md" fixed="top">
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
         <Navbar.Brand>
           <img src={logo} alt="logo" height="45" />
