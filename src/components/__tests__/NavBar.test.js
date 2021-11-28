fd58d0bb11e37db437d098a462e8312ae293ba95
mr-fibonacci <adamlapinski1@gmail.com>
2021-11-28 12:59:46 +0000

    28b Testing in React part2

---
+++ src/components/__tests__/NavBar.test.js
@@ -0,0 +1,48 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { CurrentUserProvider } from "../../contexts/CurrentUserContext";
+import NavBar from "../NavBar";
+
+test("renders NavBar", () => {
+  render(
+    <Router>
+      <NavBar />
+    </Router>
+  );
+
+  // screen.debug();
+  const signInLink = screen.getByRole("link", { name: "Sign in" });
+  expect(signInLink).toBeInTheDocument();
+});
+
+test("renders link to the user profile for a logged in user", async () => {
+  render(
+    <Router>
+      <CurrentUserProvider>
+        <NavBar />
+      </CurrentUserProvider>
+    </Router>
+  );
+
+  const profileAvatar = await screen.findByText("Profile");
+  expect(profileAvatar).toBeInTheDocument();
+});
+
+test("renders Sign in and Sign up buttons again on log out", async () => {
+  render(
+    <Router>
+      <CurrentUserProvider>
+        <NavBar />
+      </CurrentUserProvider>
+    </Router>
+  );
+
+  const signOutLink = await screen.findByRole("link", { name: "Sign out" });
+  fireEvent.click(signOutLink);
+
+  const signInLink = await screen.findByRole("link", { name: "Sign in" });
+  const signUpLink = await screen.findByRole("link", { name: "Sign up" });
+
+  expect(signInLink).toBeInTheDocument();
+  expect(signUpLink).toBeInTheDocument();
+});
